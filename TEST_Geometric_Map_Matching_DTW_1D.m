95853861b1e9f309dc159b7b8e3879e4958dca51
Antonio1111111111 <tianhs6523@mails.jlu.edu.cn>
2025-10-21 22:01:44 +0800

    tianhs6523 Geometric_Map_Matching_DTW_1D function and its test code V1

---
+++ TEST_Geometric_Map_Matching_DTW_1D.m
@@ -0,0 +1,14 @@
+clc
+clear
+close all
+%% Test for the function of 1D classical dtw on 2 sequences(without figure output)
+geo_map = [1.2, 1.1, 1.3, 2.0, 5.0, 8.0, 9.0, 8.5, 4.0, 2.1, 1.4, 1.3, 1.5];
+live_reading = [5.2, 7.8, 9.1, 9.0, 8.2, 3.5];
+[dist, loc] = Geomectric_Map_Matching_DTW_1D(geo_map, live_reading);
+fprintf("The minimum distanse is %d . \n", dist);
+fprintf('The target sequence on the geo map is:\n');
+disp(loc);
+%% Test for the function of 1D classical dtw on 2 sequences(with figure output)
+geo_map = [1.2, 1.1, 1.3, 2.0, 5.0, 8.0, 9.0, 8.5, 4.0, 2.1, 1.4, 1.3, 1.5];
+live_reading = [5.2, 7.8, 9.1, 9.0, 8.2, 3.5];
+[dist, loc] = Geomectric_Map_Matching_DTW_1D(geo_map, live_reading, true);
